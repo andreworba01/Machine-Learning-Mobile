f5f115906d4d48837f6a5b26cd044f6d4a62ff14
andreworba01 <136288676+andreworba01@users.noreply.github.com>
2023-07-01 23:14:31 -0500

    script.js

--- script.js
+++ script.js
@@ -55,41 +55,33 @@ let predict = false;
  **/
 
 async function loadMobileNetFeatureModel() {
-  const URL =
-    "https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v3_small_100_224/feature_vector/5/default/1";
+  const URL = "https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v3_small_100_224/feature_vector/5/default/1";
   mobilenet = await tf.loadGraphModel(URL, { fromTFHub: true });
   STATUS.innerText = "Model loaded successfully!";
-
-  // Warm up the model by passing zeros through it once.
-  tf.tidy(function () {
-    let answer = mobilenet.predict(
-      tf.zeros([1, MOBILE_NET_INPUT_HEIGHT, MOBILE_NET_INPUT_WIDTH, 3])
-    );
-    console.log(answer.shape);
-  });
 }
 
 loadMobileNetFeatureModel();
 
 let model = tf.sequential();
 model.add(
-  tf.layers.dense({ inputShape: [1024], units: 128, activation: "relu" })
-);
-model.add(
+  mobilenet,
+  tf.layers.dense({ units: 256, activation: "relu" }),
+  tf.layers.dropout({ rate: 0.5 }),
   tf.layers.dense({ units: CLASS_NAMES.length, activation: "softmax" })
 );
 
 model.summary();
 
+// Adjust the learning rate based on your specific problem
+const learningRate = 0.001;
+
+// Use a suitable optimizer and learning rate
+const optimizer = tf.train.adam(learningRate);
+
 // Compile the model with the defined optimizer and specify a loss function to use.
 model.compile({
-  // Adam changes the learning rate over time which is useful.
-  optimizer: "adam",
-  // Use the correct loss function. If 2 classes of data, must use binaryCrossentropy.
-  // Else categoricalCrossentropy is used if more than 2 classes.
-  loss:
-    CLASS_NAMES.length === 2 ? "binaryCrossentropy" : "categoricalCrossentropy",
-  // As this is a classification problem you can record accuracy in the logs too!
+  optimizer: optimizer,
+  loss: CLASS_NAMES.length === 2 ? "binaryCrossentropy" : "categoricalCrossentropy",
   metrics: ["accuracy"],
 });
 
